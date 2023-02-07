e8851dd4e5d84783dc6a7ddcbaead38c48274d4f
Srinath <srinath.ka16@gmail.com>
2023-02-07 17:25:35 +0530

    class

---
+++ Mobile.cs
@@ -0,0 +1,35 @@
+ public class Mobile
+ {
+    public int ID{get; set;}
+
+    public string Name{get; set;}
+
+    public string ModelNumber{get; set;}
+
+    public int Warrantyduration{get; set;}
+
+    public string Colour{get; set;}
+
+    public int Storgae{get; set;}
+
+    public string Processor{get; set;}
+
+    public bool ExtraSim{get; set;}
+
+    public float Ratings{get; set;}
+
+    public long Imei{get; set;}
+
+    public string MadeIn{get; set;}
+
+}
+
+Static void Main (Strings[] args)
+{
+   Mobile mobile = new Mobile();
+   mobile.ID = 3344
+   mobile.Name = "Nothing";
+   mobile.ModelNumber = "3N3O4T4H"; 
+
+
+}
